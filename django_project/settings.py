0fe1723862a8f7cd38dcfa217a5d498d9e1a7e41
estein1988 <estein1988@gmail.com>
2020-11-22 09:40:28 -0700

    Create .env file.

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p9rnco1@==u0s1^q4d7d)$#v3a)y4k^o&&6@4#%31ox&_$zmy5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,6 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'home_listings',
         'USER': 'postgres',
+        'PASSWORD': config('PG_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
